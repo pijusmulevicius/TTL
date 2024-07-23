89e2528a9a5b5203b5dda3f9b0ce47fd8edca9a7
Pijus Mulevicius <pijusmul@gmail.com>
2024-07-23 22:44:01 +0300

    initial commit

---
+++ src/main/java/com/ttl/ttlcore/TtlcoreApplication.java
@@ -0,0 +1,13 @@
+package com.ttl.ttlcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtlcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtlcoreApplication.class, args);
+	}
+
+}
